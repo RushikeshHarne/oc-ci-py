17a91135ee4fafe3f1a459dd3ba1b4a5b412d320
Rushikesh Harne <161678001+RushikeshHarne@users.noreply.github.com>
2025-11-17 19:44:34 +0530

    Create test.py

---
+++ test.py
@@ -0,0 +1,8 @@
+# This is a comment. Python ignores lines starting with #
+
+# The print() function displays text on the console.
+print("Hello, World!")
+
+# You can print any string or variable.
+message = "This is a simple Python output."
+print(message)
