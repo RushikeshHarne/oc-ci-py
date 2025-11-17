dfe2c09b13476773c04d4cc9c7315211ac87e1ce
Rushikesh Harne <161678001+RushikeshHarne@users.noreply.github.com>
2025-11-17 19:49:53 +0530

    Update and rename test.py to test.html

--- test.py
+++
@@ -1,8 +0,0 @@
-# This is a comment. Python ignores lines starting with #
-
-# The print() function displays text on the console.
-print("Hello, World!")
-
-# You can print any string or variable.
-message = "This is a simple Python output."
-print(message)
